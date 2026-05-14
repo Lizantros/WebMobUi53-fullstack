07f112e819054e2c579f8100f6fc85ed55765b31
Lizantros <loic63130@gmail.com>
2026-05-14 17:08:55 +0200

    ajustements de la session sanctum pour le frontend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'abilities' => CheckAbilities::class,
             'ability' => CheckForAnyAbility::class,
         ]);
-        $middleware->prependToGroup('api', EnsureFrontendRequestsAreStateful::class);
+        $middleware->api(prepend: [
+            EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
